146483a914bb4f49c900a0b256e41dda1bd3af54
ekziman <ekziman@gmail.com>
2022-09-08 21:30:15 +0500

    v 0.91

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ function scrollHeader() {
     const header = document.getElementById('header')
 
     if(this.scrollY >= 50) header.classList.add('scroll-header');
-    else header.classList.remove(scroll-header)
+    else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
@@ -74,9 +74,32 @@ function activeFeatured() {
 linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 /*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
 
+  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
